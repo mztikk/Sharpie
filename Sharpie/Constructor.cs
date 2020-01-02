866d8e7bc37f0258c3e346698e82fd818cf14d2c
mztikk <nevigskcuforez@gmail.com>
2020-01-02 10:45:43 +0100

    added constructor

---
+++ Sharpie/Constructor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Sharpie
+{
+    public class Constructor
+    {
+        public Constructor(Accessibility accessibility, string name, IEnumerable<Argument> arguments, string body)
+        {
+            Accessibility = accessibility;
+            Name = name;
+            Arguments = arguments;
+            Body = body;
+        }
+
+        public readonly Accessibility Accessibility;
+        public readonly string Name;
+        public readonly IEnumerable<Argument> Arguments;
+        public readonly string Body;
+    }
+}
