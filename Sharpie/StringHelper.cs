2bb6f6dd9405bf74367ebca79c3ee053299cb573
mztikk <nevigskcuforez@gmail.com>
2020-01-02 10:44:03 +0100

    added stringhelper

---
+++ Sharpie/StringHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Sharpie
+{
+    public static class StringHelper
+    {
+        public static IEnumerable<string> GetLines(this string s)
+        {
+            using (StringReader reader = new StringReader(s))
+            {
+                while (true)
+                {
+                    string line = reader.ReadLine();
+                    if (line is null)
+                    {
+                        break;
+                    }
+
+                    yield return line;
+                }
+            }
+        }
+    }
+}
