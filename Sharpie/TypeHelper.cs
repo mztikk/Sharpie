31083f7fd9396ca56f38fc84439b8ff35fefe19a
mztikk <nevigskcuforez@gmail.com>
2020-01-02 10:37:55 +0100

    added TypeHelper

---
+++ Sharpie/TypeHelper.cs
@@ -0,0 +1,24 @@
+﻿namespace Sharpie
+{
+    public static class TypeHelper
+    {
+        public static string GetCSharpTypeName(string s) => s switch
+        {
+            "SByte" => "sbyte",
+            "Byte" => "byte",
+            "Int16" => "short",
+            "UInt16" => "ushort",
+            "Int32" => "int",
+            "UInt32" => "uint",
+            "Int64" => "long",
+            "UInt64" => "ulong",
+            "Single" => "float",
+            "Double" => "double",
+            "Boolean" => "bool",
+            "Char" => "char",
+            "String" => "string",
+            "Object" => "object",
+            _ => s,
+        };
+    }
+}
