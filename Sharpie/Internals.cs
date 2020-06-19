7c201e1dff29f50c4802240d124205470eb5c56a
mztikk <nevigskcuforez@gmail.com>
2020-06-19 22:29:09 +0200

    locking hashset instead of concurrentdict

--- Sharpie/Internals.cs
+++ Sharpie/Internals.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Concurrent;
+using System.Collections.Generic;
 using RFReborn;
 using RFReborn.Extensions;
 using RFReborn.RandomR;
@@ -8,26 +8,29 @@ namespace Sharpie
 {
     internal static class Internals
     {
-        internal const byte NULL_BYTE = 0x0;
-
         internal static Random s_random = new CryptoRandom();
         internal const string Alphabet = StringR.AlphabetUpper + StringR.AlphabetLower;
 
-        private static readonly ConcurrentDictionary<string, byte> s_usedNames = new ConcurrentDictionary<string, byte>();
+        private static readonly HashSet<string> s_usedNames = new HashSet<string>();
+        private static readonly object s_usedNamesLocker = new object();
 
         internal static string GetIdentifierName(int len = 8)
         {
             while (true)
             {
                 string gen = s_random.NextString(Alphabet, len);
-                if (s_usedNames.ContainsKey(gen))
-                {
-                    continue;
-                }
 
-                if (s_usedNames.TryAdd(gen, NULL_BYTE))
+                lock (s_usedNamesLocker)
                 {
-                    return gen;
+                    if (s_usedNames.Contains(gen))
+                    {
+                        continue;
+                    }
+
+                    if (s_usedNames.Add(gen))
+                    {
+                        return gen;
+                    }
                 }
             }
         }
