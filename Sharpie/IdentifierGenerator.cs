e4a0c4edfd1b7f415afe6a1fc51a905f71f14af5
mztikk <nevigskcuforez@gmail.com>
2020-06-20 19:27:33 +0200

    added IdentifierGenerator

---
+++ Sharpie/IdentifierGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using RFReborn.Extensions;
+
+namespace Sharpie
+{
+    public static class IdentifierGenerator
+    {
+        private static readonly HashSet<string> s_usedNames = new HashSet<string>();
+        private static readonly object s_usedNamesLocker = new object();
+
+        internal static string Get(int len = 8)
+        {
+            while (true)
+            {
+                string gen = Internals.s_random.NextString(Internals.Alphabet, len);
+
+                lock (s_usedNamesLocker)
+                {
+                    if (s_usedNames.Contains(gen))
+                    {
+                        continue;
+                    }
+
+                    if (s_usedNames.Add(gen))
+                    {
+                        return gen;
+                    }
+                }
+            }
+        }
+    }
+}
