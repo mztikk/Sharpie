9d04ee1ca956c1a530d0edc8ce066f837a81d9c0
mztikk <nevigskcuforez@gmail.com>
2020-06-20 08:53:32 +0200

    xml docs for StringHelper

--- Sharpie/StringHelper.cs
+++ Sharpie/StringHelper.cs
@@ -8,6 +8,10 @@ namespace Sharpie
 {
     public static class StringHelper
     {
+        /// <summary>
+        /// Yields every line inside a string
+        /// </summary>
+        /// <param name="s">String to get lines from</param>
         public static IEnumerable<string> GetLines(this string s)
         {
             using (StringReader reader = new StringReader(s))
@@ -20,6 +24,10 @@ namespace Sharpie
             }
         }
 
+        /// <summary>
+        /// Creates an Action that calls <see cref="IndentedStreamWriter.WriteLine(string)"/> on every line of <see langword="abstract"/>string
+        /// </summary>
+        /// <param name="s">String to turn into callable action</param>
         public static Action<IndentedStreamWriter> StringToCall(string s) => (IndentedStreamWriter writer) => s.GetLines().Call(writer.WriteLine);
     }
 }
