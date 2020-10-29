aa237d78597d3cf6f69105bdf2bbc5bc5fc9f520
mztikk <nevigskcuforez@gmail.com>
2020-10-29 22:08:24 +0100

    added if + writer

---
+++ Sharpie/If.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Sharpie.Writer;
+
+namespace Sharpie
+{
+    public readonly struct If
+    {
+        public readonly string Condition;
+        public readonly Action<BodyWriter> Body;
+
+        public If(string condition, Action<BodyWriter> body)
+        {
+            Condition = condition;
+            Body = body;
+        }
+
+        public If(string condition, string body) : this(condition, StringHelper.StringToBodyWriter(body)) { }
+    }
+}
---
+++ Sharpie/Writer/IfWriter.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+
+namespace Sharpie.Writer
+{
+    internal static class IfWriter
+    {
+        public static async Task WriteAsync(If @if, IndentedStreamWriter writer)
+        {
+            await writer.WriteLineAsync($"if ({@if.Condition})");
+            await writer.WriteLineAsync("{").ConfigureAwait(false);
+            writer.IndentationLevel++;
+
+            var bodyWriter = new BodyWriter(writer);
+            @if.Body(bodyWriter);
+
+            writer.IndentationLevel--;
+            await writer.WriteLineAsync("}").ConfigureAwait(false);
+        }
+
+        public static void Write(If @if, IndentedStreamWriter writer)
+        {
+            writer.WriteLine($"if ({@if.Condition})");
+            writer.WriteLine("{");
+            writer.IndentationLevel++;
+
+            var bodyWriter = new BodyWriter(writer);
+            @if.Body(bodyWriter);
+
+            writer.IndentationLevel--;
+            writer.WriteLine("}");
+        }
+    }
+}
