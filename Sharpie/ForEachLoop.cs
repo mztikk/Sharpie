783fc193811196e3f69f7edf98c016cef3805252
mztikk <nevigskcuforez@gmail.com>
2020-10-29 22:04:57 +0100

    added foreachloop and writer

---
+++ Sharpie/ForEachLoop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Sharpie.Writer;
+
+namespace Sharpie
+{
+    public readonly struct ForEachLoop
+    {
+        public readonly string Item;
+        public readonly string Collection;
+        public readonly Action<BodyWriter> Body;
+
+        public ForEachLoop(string item, string collection, Action<BodyWriter> body)
+        {
+            Item = item;
+            Collection = collection;
+            Body = body;
+        }
+
+        public ForEachLoop(string item, string collection, string body) : this(item, collection, StringHelper.StringToBodyWriter(body)) { }
+
+        public ForEachLoop(string collection, Action<BodyWriter> body) : this("var item", collection, body) { }
+        public ForEachLoop(string collection, string body) : this(collection, StringHelper.StringToBodyWriter(body)) { }
+    }
+}
---
+++ Sharpie/Writer/ForEachLoopWriter.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+
+namespace Sharpie.Writer
+{
+    internal static class ForEachLoopWriter
+    {
+        public static async Task WriteAsync(ForEachLoop forEachLoop, IndentedStreamWriter writer)
+        {
+            await writer.WriteLineAsync($"foreach ({forEachLoop.Item} in {forEachLoop.Collection})");
+            await writer.WriteLineAsync("{").ConfigureAwait(false);
+            writer.IndentationLevel++;
+
+            var bodyWriter = new BodyWriter(writer);
+            forEachLoop.Body(bodyWriter);
+
+            writer.IndentationLevel--;
+            await writer.WriteLineAsync("}").ConfigureAwait(false);
+        }
+
+        public static void Write(ForEachLoop forEachLoop, IndentedStreamWriter writer)
+        {
+            writer.WriteLine($"foreach ({forEachLoop.Item} in {forEachLoop.Collection})");
+            writer.WriteLine("{");
+            writer.IndentationLevel++;
+
+            var bodyWriter = new BodyWriter(writer);
+            forEachLoop.Body(bodyWriter);
+
+            writer.IndentationLevel--;
+            writer.WriteLine("}");
+        }
+    }
+}
