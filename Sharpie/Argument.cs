ea977dc065746a809e17280d091c3abbc1c58aad
mztikk <nevigskcuforez@gmail.com>
2020-10-31 07:33:05 +0100

    added nullable default value for argument

--- Sharpie/Argument.cs
+++ Sharpie/Argument.cs
@@ -2,15 +2,19 @@
 {
     public readonly struct Argument
     {
-        public Argument(string type, string name)
+        public Argument(string type, string name, string? defaultValue)
         {
             Type = type;
             Name = name;
+            DefaultValue = defaultValue;
         }
+
+        public Argument(string type, string name) : this(type, name, null) { }
 
         public string Type { get; }
         public string Name { get; }
+        public string? DefaultValue { get; }
 
-        public override string ToString() => Type + " " + Name;
+        public override string ToString() => DefaultValue is { } ? $"{Type} {Name} = {DefaultValue}" : Type + " " + Name;
     }
 }
