ff20785dac2d875be8d239a9a3efe66f72f2c0d4
mztikk <nevigskcuforez@gmail.com>
2021-01-16 16:54:58 +0100

    added test project and method modifier tests

---
+++ SharpieTests/BaseSharpieTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using Sharpie.Writer;
+
+namespace SharpieTests
+{
+    public class BaseSharpieTest
+    {
+        protected static Stream GetStream() => new MemoryStream();
+
+        protected static IndentedStreamWriter GetIndentedStreamWriter() => new IndentedStreamWriter((GetStream()));
+
+        protected static string GetString(Stream stream)
+        {
+            stream.Position = 0;
+            using var reader = new StreamReader(stream, leaveOpen: true);
+            return reader.ReadToEnd();
+        }
+
+        protected static string GetString(Action<IndentedStreamWriter> action)
+        {
+            using Stream stream = GetStream();
+            using IndentedStreamWriter indentedWriter = new IndentedStreamWriter(stream);
+            action(indentedWriter);
+            return GetString(stream);
+        }
+    }
+}
---
+++ SharpieTests/MethodTests/MethodModifierTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sharpie;
+using Sharpie.Writer.Methods;
+
+namespace SharpieTests.MethodTests
+{
+    [TestClass]
+    public class MethodModifierTests : BaseSharpieTest
+    {
+        private Method GetStaticMethod() => new Method(Microsoft.CodeAnalysis.Accessibility.Public, true, false, "void", "StaticMethod", Array.Empty<Parameter>(), string.Empty);
+        private Method GetNonStaticMethod() => new Method(Microsoft.CodeAnalysis.Accessibility.Public, false, false, "void", "NonStaticMethod", Array.Empty<Parameter>(), string.Empty);
+
+        private Method GetPublicMethod() => new Method(Microsoft.CodeAnalysis.Accessibility.Public, true, false, "void", "StaticMethod", Array.Empty<Parameter>(), string.Empty);
+        private Method GetPrivateMethod() => new Method(Microsoft.CodeAnalysis.Accessibility.Private, false, false, "void", "NonStaticMethod", Array.Empty<Parameter>(), string.Empty);
+
+        [TestMethod]
+        public void ContainsStatic() => Contains(GetStaticMethod, "static");
+        [TestMethod]
+        public void DoesntContainStatic() => DoesNotMatch(GetNonStaticMethod, new Regex(".*static.*"));
+
+        [TestMethod]
+        public void ContainsPublic() => Contains(GetPublicMethod, "public");
+        [TestMethod]
+        public void DoesntContainPublic() => DoesNotMatch(GetPrivateMethod, new Regex(".*public.*"));
+
+        private void Contains(Func<Method> methodFactory, string substring) => StringAssert.Contains(ModifierTest(methodFactory), substring);
+        private void Matches(Func<Method> methodFactory, Regex pattern) => StringAssert.Matches(ModifierTest(methodFactory), pattern);
+        private void DoesNotMatch(Func<Method> methodFactory, Regex pattern) => StringAssert.DoesNotMatch(ModifierTest(methodFactory), pattern);
+
+        private static string ModifierTest(Func<Method> methodFactory) => GetString((indentedWriter) => new MethodModifierWriter(indentedWriter, methodFactory()).Make());
+    }
+}
